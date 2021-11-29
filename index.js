5645619f7a1e61b715a81c979ef63922d25bb004
Tkayy729 <emmanueltweneboah71@gmail.com>
2021-11-29 17:19:45 +0000

    implemented functions for rolling the dice

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ document.querySelector(".btn--roll").addEventListener("click", function () {
   var diceDom = document.querySelector(".dice");
   diceDom.style.display = "block";
   diceDom.src = "assets/dice-" + dice + ".png";
+
+  //update randomScore if dice is not equal to 1
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+  }
 });
 
 // dice = Math.floor(Math.random() * 6) + 1;
