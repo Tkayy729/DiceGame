9c44bdd1373363475e12aadf1054a6cfaa151d4c
Tkayy729 <emmanueltweneboah71@gmail.com>
2021-11-29 15:29:58 +0000

    added a function to generate random numbers with respective  display of dice image

---
+++ index.js
@@ -0,0 +1,26 @@
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn--roll").addEventListener("click", function () {
+  //Random number selection
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // display the random number
+  var diceDom = document.querySelector(".dice");
+  diceDom.style.display = "block";
+  diceDom.src = "assets/dice-" + dice + ".png";
+});
+
+// dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+// document.querySelector("#current--0").textContent = dice;
